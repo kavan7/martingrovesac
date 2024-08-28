013a455d1e0bce4c0efdb29c40112f437c64d19a
kavan7 <abeyratnekavan@gmail.com>
2024-08-27 22:28:33 -0400

    adawaf

---
+++ app/about/page.tsx
@@ -0,0 +1,33 @@
+import { Hero } from "@/components/Hero";
+import { FloatingNav } from "@/components/ui/floating-navbar";
+import { IconHome, IconMessage, IconUser } from "@tabler/icons-react";
+import Image from "next/image";
+
+const navItems = [
+  {
+    name: "Home",
+    link: "/",
+    icon: <IconHome className="h-4 w-4 text-neutral-500 dark:text-white" />,
+  },
+  {
+    name: "About",
+    link: "/about",
+    icon: <IconUser className="h-4 w-4 text-neutral-500 dark:text-white" />,
+  },
+  {
+    name: "Team",
+    link: "/team",
+    icon: (
+      <IconMessage className="h-4 w-4 text-neutral-500 dark:text-white" />
+    ),
+  },
+];
+
+export default function Home() {
+  return (
+    <main className="">
+      <FloatingNav navItems={navItems}/>
+      <Hero/>
+    </main>
+  );
+}
